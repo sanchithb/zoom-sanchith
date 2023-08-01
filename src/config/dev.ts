ce314e12ba4f774807a2711c5e8f9269303e2cbd
sanchithb <55482452+sanchithb@users.noreply.github.com>
2023-08-01 14:13:11 +0530

    Config

---
+++ src/config/dev.ts
@@ -0,0 +1,16 @@
+import { getExploreName } from '../utils/platform';
+
+export const devConfig = {
+  sdkKey: 'nGmH30wIhv1OrdmFGVUMUY3aXL6ml8HZNSPK',
+  sdkSecret: '6VTPQVMzRxq1WBKhy0cHrecbzZ3Tgd0bSqoR',
+  webEndpoint: 'go.zoom.us',
+  topic: 'CR1-Del1234567855',
+  name: `Chethan`,
+  password: 'Onnet@123',
+  signature:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoibkdtSDMwd0lodjFPcmRtRkdWVU1VWTNhWEw2bWw4SFpOU1BLIiwicm9sZV90eXBlIjowLCJjbG91ZF9yZWNvcmRpbmdfZWxlY3Rpb24iOjAsInRwYyI6IkNSMS1EZWwxMjM0NTY3ODU1IiwiZ2VvX3JlZ2lvbnMiOiJJTiIsInNlc3Npb25fa2V5IjoiQ1IxLURlbDEyMzQ1Njc4NTUiLCJjbG91ZF9yZWNvcmRpbmdfb3B0aW9uIjowLCJ1c2VyX2lkZW50aXR5IjoiQ2hldGhhbiIsImV4cCI6MTY4OTMyNzQxNiwicHdkIjoiT25uZXRAMTIzIiwidmVyc2lvbiI6MSwiaWF0IjoxNjg5MjQxMDE2fQ.PN0XUjDMMU58LkwjLNWihaeCOtFaVUJDWNGL4xyTitE',
+  sessionKey: 'CR1-Del1234567855',
+  userIdentity: 'Chethan',
+  // role = 1 to join as host, 0 to join as attendee. The first user must join as host to start the session
+  role: 1
+};
